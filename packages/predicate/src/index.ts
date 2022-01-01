dd18d6d6eeee66e2b056bec30dfb4f5819c9000f
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2022-01-01 19:46:19 +0530

    truthy implementation

--- packages/predicate/src/index.ts
+++ packages/predicate/src/index.ts
@@ -6,38 +6,43 @@ import { P, Pa, Ps } from "./types"
 // p
 
 type PImpl = 
-  (...a: [...Operator[], Comparator, Comparand]) =>
-    (operand: Operand) => boolean
+  (...a: [] | [...Operator[], Comparator, Comparand]) =>
+    (operand: Operand) => boolean | Operand
 
 type Operand = unknown & { __isOperand: true }
 type Operator = (`${`?` | `.`}${string}` | "typeof") & { __isOperator: true }
-type Comparator = "===" & { __isComparator: true }
+type Comparator = ("===" | "!==") & { __isComparator: true }
 type Comparand = unknown & { __isComparand: true }
 
 const pImpl: PImpl = (...as) => (operand) => {
+  if (isEmpty(as)) return operand;
   let [operators, comparator, comparand] = pop2(as)
   return compare(operators.reduce(operate, operand), comparator, comparand)
 }
 const p = pImpl as P
 
 const operate = (t: Operand, o: Operator): Operand => {
-  if (/^(\.|\?\.)/.test(o)) {
+  if (isIndex(o)) {
     return get(t, o.replace(/^(\.|\?\.)/, "").replace(/\?\./g, ".").split(".")) as Operand
   }
   if (o === "typeof") {
     return (typeof t) as unknown as Operand
   }
   if (process.env.NODE_ENV === "development") {
-    assertNever(o as never)
+    assertNever(o)
   }
   return t
 }
+const isIndex = (t: Operator): t is `${`?` | `.`}${string}` & { __isOperator: true } =>
+  /^(\.|\?\.)/.test(t)
 
 const compare = (operand: Operand, comparator: Comparator, comparand: Comparand): boolean => {
   if (comparator === "===") {
     return (operand as unknown) === comparand
   }
-  
+  if (comparator === "!==") {
+    return (operand as unknown) !== comparand
+  }
   if (process.env.NODE_ENV === "development") {
     assertNever(comparator)
   }
@@ -51,12 +56,16 @@ const compare = (operand: Operand, comparator: Comparator, comparand: Comparand)
 // ps
 
 type PsImpl = 
-  (...a: [OperatorsComparator, Comparand]) =>
-    (operand: Operand) => boolean
+  (...a: [] | [OperatorsComparator, Comparand]) =>
+    (operand: Operand) => boolean | Operand
 type OperatorsComparator = string & { __isOperatorsComparator: true }
 
-const psImpl: PsImpl = (a0, a1) =>
-  pImpl(...(a0.split(" ") as [...Operator[], Comparator]), a1)
+const psImpl: PsImpl = (...a) => {
+  if (isEmpty(a)) return pImpl();
+  let [a0, a1] = a;
+  return pImpl(...(a0.split(" ") as [...Operator[], Comparator]), a1)
+}
+  
 const ps = psImpl as Ps;
 
 
@@ -79,7 +88,10 @@ const get = (t: unknown, ks: string[]): unknown => {
   if (t === undefined || t === null) return t;
   return get((t as never)[k], ks_)
 }
- 
+
+const isEmpty = <T extends [] | unknown[]>(xs: T): xs is T & [] =>
+  xs.length === 0
+
 const pop2 = <A extends unknown[], B, C>(xs: [...A, B, C]) =>
   [xs.slice(0, -2), xs.slice(-2)[0], xs.slice(-1)[0]] as [A, B, C]
 
